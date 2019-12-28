f561e0300e8d3daeb044ea1be72ce8ec82f91c73
victor-7-7 <Victor.7.7@ya.ru>
2019-12-28 14:23:53 +0700

    hometask_2. Fix Utils#parseFullName()

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -10,11 +10,9 @@ data class User (
     var avatar:String?,
     var rating:Int = 0,
     var respect:Int = 0,
-    val lastVisit:Date? = null,
+    val lastVisit:Date? = Date(),
     val isOnline:Boolean = false
     ) {
-
-    var introBit:String
 
     constructor(id:String, firstName:String?, lastName:String?)
             : this(id = id, firstName = firstName,
@@ -22,30 +20,16 @@ data class User (
 
     constructor(id:String): this(id, "John", "Doe")
 
-    init {
-        introBit = getIntro()
-//        println("\nIt's Alive!!!\n${if (lastName === "Doe")
-//                                "His name id $firstName $lastName"
-//                                else "And his name is $firstName $lastName!!!"}\n" +
-//                                introBit
-//        )
-    }
-
-    private fun getIntro() = """
-        la la la!!!
-        topola^^^        
-        It's Ok!""".trimIndent()
-
     fun printMe() = println("""
-            id: $id
-            firstName: $firstName
-            lastName: $lastName
-            avatar: $avatar
-            rating: $rating
-            respect: $respect
-            lastVisit: $lastVisit
-            isOnline: $isOnline            
-        """.trimIndent())
+        id: $id
+        firstName: $firstName
+        lastName: $lastName
+        avatar: $avatar
+        rating: $rating
+        respect: $respect
+        lastVisit: $lastVisit
+        isOnline: $isOnline            
+    """.trimIndent())
 
     companion object Factory {
         private var lastId:Int = -1
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,11 +2,10 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName:String?): Pair<String?, String?> {
-        //TODO FIX ME!!!
-        val parts:List<String>? = fullName?.split(" ")
+        if (fullName.isNullOrBlank()) return "Anonymous" to "Anonym"
+        val parts:List<String>? = fullName.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
-//        return Pair(firstName, lastName)
         return firstName to lastName
     }
 
