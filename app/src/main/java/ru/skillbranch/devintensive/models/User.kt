5cf0ffc5bcfaa018a29c821a360647bb599cd0f3
victor-7-7 <Victor.7.7@ya.ru>
2020-01-02 20:27:48 +0700

    hometask_2. User Builder done.

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
@@ -27,7 +28,7 @@ data class User (
         avatar: $avatar
         rating: $rating
         respect: $respect
-        lastVisit: $lastVisit
+        lastVisit: ${lastVisit?.humanizeDiff()}
         isOnline: $isOnline            
     """.trimIndent())
 
@@ -38,6 +39,36 @@ data class User (
             val (firstName, lastName) = Utils.parseFullName(fullName)
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
+    }
+
+    class Builder(
+        private var id:String = "0",
+        private var firstName:String? = null,
+        private var lastName:String? = null,
+        private var avatar:String? = null,
+        private var rating:Int = 0,
+        private var respect:Int = 0,
+        private var lastVisit:Date? = Date(),
+        private var isOnline:Boolean = false
+    ){
+        fun id(id:String) = apply { this.id = id }
+
+        fun firstName(firstName:String) = apply { this.firstName = firstName }
+
+        fun lastName(lastName:String) = apply { this.lastName = lastName }
+
+        fun avatar(avatar:String) = apply { this.avatar = avatar }
+
+        fun rating(rating: Int) = apply { this.rating = rating }
+
+        fun respect(respect: Int) = apply { this.respect = respect }
+
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = User(id, firstName, lastName, avatar, rating,
+                            respect, lastVisit, isOnline)
     }
 
 }
