63319502c5bb69a6396fc5d710b64e74468c106e
victor-7-7 <Victor.7.7@ya.ru>
2020-01-04 12:43:31 +0700

    hometask_2. Amend parseFullName and toInitials utils .

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,10 +2,9 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName:String?): Pair<String?, String?> {
-        if (fullName.isNullOrBlank()) return "null" to "null"
-        val parts:List<String>? = fullName.trim().split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val parts = fullName?.trim()?.split(" ")
+        val firstName = parts?.getOrNull(0)?.ifEmpty { null }
+        val lastName = parts?.getOrNull(1)?.ifEmpty { null }
         return firstName to lastName
     }
 
@@ -38,12 +37,11 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        var initials = ""
-        if (!firstName.isNullOrBlank() && firstName.trim()[0].isLetter())
-            initials = firstName.trim()[0].toUpperCase().toString()
-        if (!lastName.isNullOrBlank() && lastName.trim()[0].isLetter())
-            initials += lastName.trim()[0].toUpperCase().toString()
-        if (initials == "") return "null" else return initials
+        val fni = firstName.orEmpty().trim().getOrNull(0)
+                        ?.toUpperCase()?.toString() ?: ""
+        val lni = lastName.orEmpty().trim().getOrNull(0)
+                        ?.toUpperCase()?.toString() ?: ""
+        return "$fni$lni".ifEmpty { null }
     }
 }
 
